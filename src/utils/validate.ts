082240e3528cef3bad5562567d14dcd835754afd
Shuten-doji-ex <newrunone00@gmail.com>
2026-05-20 15:20:02 +0900

    feat: 로그인 페이지 유효성 검사 및 에러 스타일 추가

---
+++ src/utils/validate.ts
@@ -0,0 +1,27 @@
+export type UserSignInformation = {
+  email: string;
+  password: string;
+};
+
+function validateUser(values: UserSignInformation) {
+  const errors = {
+    email: '',
+    password: ''
+  };
+
+  if (values.email && !/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i.test(values.email)) {
+    errors.email = '이메일 형식이 올바르지 않습니다.';
+  }
+
+  if (!(values.password.length >= 0 && values.password.length <= 20)) {
+    errors.password = '비밀번호는 0자 이상 20자 이하여야 합니다.';
+  }
+
+  return errors;
+}
+
+  function validateSignIn(values: UserSignInformation) {
+    return validateUser(values);
+    };
+
+export { validateSignIn };
