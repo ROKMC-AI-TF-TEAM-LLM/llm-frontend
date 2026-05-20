a36e3c0f63f79e1ee83033e7fc107972b82384cc
Shuten-doji-ex <newrunone00@gmail.com>
2026-05-20 22:03:55 +0900

    로그인 화면 수정

--- src/ui/components/logincard.tsx
+++ src/ui/components/logincard.tsx
@@ -2,14 +2,14 @@ import { useNavigate } from 'react-router-dom'
 const LoginCard = () => {
   const navigate = useNavigate()
   return (
-    <div className="card flex w-240 min-h-143.25 overflow-hidden">
+    <div className="card flex w-200 min-h-105 overflow-hidden">
 
       <div className="flex w-1/2 flex-col items-center justify-center gap-4 border-r border-surface-border px-12 py-12">
 
-        <div className="w-[7.1vw] max-w-25 h-[7.1vw] max-h-25 rounded-full border --color-brand-dark: #8b0000;" />
+        <div className="w-[7.1vw] max-w-20 h-[7.1vw] max-h-20 rounded-full border --color-brand-dark: #8b0000;" />
 
         <div className="text-center leading-none">
-          <p className="text-7xl font-black tracking-tight text-brand text-glow-brand">
+          <p className="text-5xl font-black tracking-tight text-brand text-glow-brand">
             ROKMC <br /> LLM
           </p>
         </div>
@@ -21,14 +21,14 @@ const LoginCard = () => {
 
       <div className="flex flex-col items-center justify-center gap-4 px-12 py-12 w-1/2">
 
-        <h2 className="text-xl font-bold text-text-primary text-[27px]!">플랫폼 접속</h2>
+        <h2 className="text-xl font-bold text-text-primary text-[22px]!">플랫폼 접속</h2>
 
         <p className="text-xs text-text-muted text-center -mt-2">
           군 인트라넷 계정으로 자동 인증됩니다.
         </p>
 
         <button 
-          className="btn-primary w-full h-[9vh] max-h-17.25 rounded-[20px]! text-[22px]!" 
+          className="btn-primary w-full h-[9vh] max-h-14 rounded-[20px]! text-[20px]!" 
           onClick={() => navigate('/signin')}
         >
           시작하기
